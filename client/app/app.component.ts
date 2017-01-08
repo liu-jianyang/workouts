10c4e220ceda3b63a006118c4cca2bc905beec87
liu-jianyang <liu-jianyang@users.noreply.github.com>
2017-01-07 21:35:17 -0800

    Css polish (#5)
    * Hover over changes paths. Updated tooltip
    
    * Added routes, navigation bar
    
    * One off with additional code
    
    * Moved nav bar to the top
    
    * V2 complete

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
+import {NgbTooltipConfig} from '@ng-bootstrap/ng-bootstrap';
 
-import { Exercise } from './exercise';
-import { ExerciseComponent } from './exercise.component';
-import { ExerciseGridComponent } from './exercise-grid.component';
+  
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
-  styleUrls: ['app/app.component.css']
+  styleUrls: ['app/app.component.css'],
+  providers: [NgbTooltipConfig]
 })
 
 export class AppComponent {
   title = 'Bodyweight exercises';
-  selectedExercise: Exercise;
-  onSelect(exercise: Exercise): void {
-    // console.log('hi');
-	  this.selectedExercise = exercise;
-	}
+  constructor(config: NgbTooltipConfig) {
+    // customize default values of tooltips used by this component tree
+    config.placement = 'right';
+    config.container = 'body';
+  }
+
+  ngAfterViewInit() {
+    let navBar = document.getElementById('nav-bar');
+    let container = document.getElementById('router-container');
+    container.style.marginTop = navBar.offsetHeight + 'px';
+  }
 }
